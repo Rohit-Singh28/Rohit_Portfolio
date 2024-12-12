814b5ec3c7c16b85e995f9edb733df54f8283bed
Rohit-Singh28 <rohitsingh450718@gmail.com>
2024-12-12 22:41:03 +0530

    feat:added tech in technology section

--- Portfolio/src/components/Technology.jsx
+++ Portfolio/src/components/Technology.jsx
@@ -7,7 +7,9 @@ import { FaHtml5 } from "react-icons/fa";
 import { DiCss3 } from "react-icons/di";
 import { IoLogoJavascript } from "react-icons/io";
 import { RiNextjsFill } from "react-icons/ri";
+import { RiFirebaseFill } from "react-icons/ri";
 import { motion } from "framer-motion"
+import { SiClerk } from "react-icons/si";
 
 const Technology = () => {
 
@@ -25,7 +27,7 @@ const Technology = () => {
     })
 
     return (
-        <div className='px-8 md:px-32  mt-20 mb-36 text-slate-300 '>
+        <div className='px-8 md:px-44  mt-20 mb-36 text-slate-300 '>
             <motion.h1
                 initial={{ y: -100, opacity: 0 }}
                 whileInView={{ y: 0, opacity: 1 }}
@@ -38,6 +40,7 @@ const Technology = () => {
                 viewport={{ once: true }}
                 transition={{ duration: 1, delay: 1 }}
                 className='flex flex-wrap items-center justify-center gap-6 md:gap-16 mt-24'>
+
                 <motion.div
                     variants={IconAni(1.9)}
                     initial="hidden"
@@ -45,6 +48,7 @@ const Technology = () => {
                     className='border border-slate-700 rounded-xl shadow-sm shadow-slate-400 p-4  text-3xl md:text-6xl'>
                     <RiNextjsFill className='' />
                 </motion.div>
+
                 <motion.div
                     variants={IconAni(2.6)}
                     initial="hidden"
@@ -52,6 +56,7 @@ const Technology = () => {
                     className='border border-slate-700 rounded-xl shadow-sm shadow-slate-400 p-4  text-3xl md:text-6xl'>
                     <RiReactjsLine className='text-blue-400' />
                 </motion.div>
+
                 <motion.div
                     variants={IconAni(2.3)}
                     initial="hidden"
@@ -59,14 +64,31 @@ const Technology = () => {
                     className='border border-slate-700 rounded-xl shadow-sm shadow-slate-400 p-4  text-3xl md:text-6xl'>
                     <SiMongodb className='text-green-500' />
                 </motion.div>
+
+                <motion.div
+                    variants={IconAni(1.8)}
+                    initial="hidden"
+                    animate="visible"
+                    className='border border-slate-700 rounded-xl shadow-sm shadow-slate-400 p-4  text-3xl md:text-6xl'>
+                    <RiFirebaseFill  className='text-[#FFCC2F]' />
+                </motion.div>
+
                 <motion.div
                     variants={IconAni(2)}
                     initial="hidden"
                     animate="visible"
                     className='border border-slate-700 rounded-xl shadow-sm shadow-slate-400 p-4  text-3xl md:text-6xl'>
                     <FaNodeJs className='text-green-700' />
-
                 </motion.div>
+
+                <motion.div
+                    variants={IconAni(2)}
+                    initial="hidden"
+                    animate="visible"
+                    className='border border-slate-700 rounded-xl shadow-sm shadow-slate-400 p-4  text-3xl md:text-6xl'>
+                    <SiClerk  className='' />
+                </motion.div>
+
                 <motion.div
                     variants={IconAni(2.9)}
                     initial="hidden"
